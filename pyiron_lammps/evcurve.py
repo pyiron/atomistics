58f9a9bee5db60fb8e4b541a88224e5cf6a71892
Jan Janßen <janssen@mpie.de>
2023-08-13 19:01:40 -0600

    more tests

--- pyiron_lammps/evcurve.py
+++ pyiron_lammps/evcurve.py
@@ -11,6 +11,149 @@ eV_div_A3_to_GPa = (
 )
 
 
+def _debye_kernel(xi):
+    return xi**3 / (np.exp(xi) - 1)
+
+
+def debye_integral(x):
+    return scipy.integrate.quad(_debye_kernel, 0, x)[0]
+
+
+def debye_function(x):
+    if hasattr(x, "__len__"):
+        return np.array([3 / xx**3 * debye_integral(xx) for xx in x])
+    return 3 / x**3 * debye_integral(x)
+
+
+class DebyeModel(object):
+    """
+    Calculate Thermodynamic Properties based on the Murnaghan output
+    """
+
+    def __init__(self, murnaghan, num_steps=50):
+        self._murnaghan = murnaghan
+
+        # self._atoms_per_cell = len(murnaghan.structure)
+        self._v_min = None
+        self._v_max = None
+        self._num_steps = None
+
+        self._volume = None
+        self._init_volume()
+
+        self.num_steps = num_steps
+        self._fit_volume = None
+        self._debye_T = None
+
+    def _init_volume(self):
+        vol = self._murnaghan["output/volume"]
+        self._v_min, self._v_max = np.min(vol), np.max(vol)
+
+    def _set_volume(self):
+        if self._v_min and self._v_max and self._num_steps:
+            self._volume = np.linspace(self._v_min, self._v_max, self._num_steps)
+            self._reset()
+            # print ('set_volume: ', self._num_steps)
+
+    @property
+    def num_steps(self):
+        return self._num_steps
+
+    @num_steps.setter
+    def num_steps(self, val):
+        self._num_steps = val
+        self._set_volume()
+
+    @property
+    def volume(self):
+        if self._volume is None:
+            self._init_volume()
+            self._set_volume()
+        return self._volume
+
+    @volume.setter
+    def volume(self, volume_lst):
+        self._volume = volume_lst
+        self._v_min = np.min(volume_lst)
+        self._v_max = np.max(volume_lst)
+        self._reset()
+
+    def _reset(self):
+        self._debye_T = None
+
+    def polynomial(self, poly_fit=None, volumes=None):
+        if poly_fit is None:
+            self._murnaghan.fit_polynomial()  # TODO: include polyfit in output
+            poly_fit = self._murnaghan.fit_dict["poly_fit"]
+        p_fit = np.poly1d(poly_fit)
+        if volumes is None:
+            return p_fit(self.volume)
+        return p_fit(volumes)
+
+    @property
+    def debye_temperature(self):
+        if self._debye_T is not None:
+            return self._debye_T
+
+        GPaTokBar = 10
+        Ang3_to_Bohr3 = (
+            scipy.constants.angstrom**3
+            / scipy.constants.physical_constants["Bohr radius"][0] ** 3
+        )
+        convert = 67.48  # conversion factor, Moruzzi Eq. (4)
+        empirical = 0.617  # empirical factor, Moruzzi Eq. (6)
+        gamma_low, gamma_high = 1, 2 / 3  # low/high T gamma
+
+        out = self._murnaghan["output"]
+        V0 = out["equilibrium_volume"]
+        B0 = out["equilibrium_bulk_modulus"]
+        Bp = out["equilibrium_b_prime"]
+
+        vol = self.volume
+
+        mass = set(self._murnaghan.structure.get_masses())
+        if len(mass) > 1:
+            raise NotImplementedError(
+                "Debye temperature only for single species systems!"
+            )
+        mass = list(mass)[0]
+
+        r0 = (3 * V0 * Ang3_to_Bohr3 / (4 * np.pi)) ** (1.0 / 3.0)
+        debye_zero = empirical * convert * np.sqrt(r0 * B0 * GPaTokBar / mass)
+        # print('r0, B0, Bp, mass, V0', r0, B0, Bp, mass, V0)
+        # print('gamma_low, gamma_high: ', gamma_low, gamma_high)
+        # print('debye_zero, V0: ', debye_zero, V0)
+        if vol is None:
+            print("WARNING: vol: ", vol)
+
+        debye_low = debye_zero * (V0 / vol) ** (-gamma_low + 0.5 * (1 + Bp))
+        debye_high = debye_zero * (V0 / vol) ** (-gamma_high + 0.5 * (1 + Bp))
+
+        self._debye_T = (debye_low, debye_high)
+        return self._debye_T
+
+    def energy_vib(self, T, debye_T=None, low_T_limit=True):
+        kB = 0.086173422 / 1000  # eV/K
+        if debye_T is None:
+            if low_T_limit:
+                debye_T = self.debye_temperature[0]  # low
+            else:
+                debye_T = self.debye_temperature[1]  # high
+        if hasattr(debye_T, "__len__"):
+            val = [
+                9.0 / 8.0 * kB * d_T
+                + T * kB * (3 * np.log(1 - np.exp(-d_T / T)) - debye_function(d_T / T))
+                for d_T in debye_T
+            ]
+            val = np.array(val)
+        else:
+            val = 9.0 / 8.0 * kB * debye_T + T * kB * (
+                3 * np.log(1 - np.exp(-debye_T / T)) - debye_function(debye_T / T)
+            )
+        atoms_per_cell = len(self._murnaghan.structure)
+        return atoms_per_cell * val
+
+
 def _strain_axes(
     structure: Atoms, axes: Literal["x", "y", "z"], volume_strain: float
 ) -> Atoms:
@@ -517,7 +660,7 @@ class EnergyVolumeCurveCalculator(object):
             "pouriertarantola",
             "vinet",
         ]:
-            self.fit_dict = self.fit_module.fit_eos_general(fittype=self.fittype)
+            self.fit_dict = self.fit_module.fit_eos_general(fittype=self.fit_type)
         else:
             raise ValueError(
                 "fit_type is unrecognized, the supported fit types are ['polynomial', "
--- tests/test_evcurve.py
+++ tests/test_evcurve.py
@@ -3,7 +3,7 @@ import unittest
 import pyiron_lammps as pyr
 
 
-def validate_elastic_constants(fit_dict):
+def validate_elastic_constants_poly(fit_dict):
     print(fit_dict)
     return [
         fit_dict['b_prime_eq'] > 3.3,
@@ -17,7 +17,77 @@ def validate_elastic_constants(fit_dict):
     ]
 
 
-class TestIntegration(unittest.TestCase):
+def validate_elastic_constants_birch(fit_dict):
+    print(fit_dict)
+    return [
+        fit_dict['b_prime_eq'] > 3.3,
+        fit_dict['b_prime_eq'] < 4.1,
+        fit_dict['bulkmodul_eq'] > 181,
+        fit_dict['bulkmodul_eq'] < 188,
+        fit_dict['energy_eq'] > -453.9,
+        fit_dict['energy_eq'] < -453.6,
+        fit_dict['volume_eq'] > 1208,
+        fit_dict['volume_eq'] < 1212,
+    ]
+
+
+def validate_elastic_constants_birchmurnaghan(fit_dict):
+    print(fit_dict)
+    return [
+        fit_dict['b_prime_eq'] > 3.3,
+        fit_dict['b_prime_eq'] < 4.1,
+        fit_dict['bulkmodul_eq'] > 181,
+        fit_dict['bulkmodul_eq'] < 188,
+        fit_dict['energy_eq'] > -453.9,
+        fit_dict['energy_eq'] < -453.6,
+        fit_dict['volume_eq'] > 1208,
+        fit_dict['volume_eq'] < 1212,
+    ]
+
+
+def validate_elastic_constants_murnaghan(fit_dict):
+    print(fit_dict)
+    return [
+        fit_dict['b_prime_eq'] > 3.3,
+        fit_dict['b_prime_eq'] < 4.1,
+        fit_dict['bulkmodul_eq'] > 181,
+        fit_dict['bulkmodul_eq'] < 188,
+        fit_dict['energy_eq'] > -453.9,
+        fit_dict['energy_eq'] < -453.6,
+        fit_dict['volume_eq'] > 1208,
+        fit_dict['volume_eq'] < 1212,
+    ]
+
+
+def validate_elastic_constants_pouriertarantola(fit_dict):
+    print(fit_dict)
+    return [
+        fit_dict['b_prime_eq'] > 3.3,
+        fit_dict['b_prime_eq'] < 4.1,
+        fit_dict['bulkmodul_eq'] > 181,
+        fit_dict['bulkmodul_eq'] < 188,
+        fit_dict['energy_eq'] > -453.9,
+        fit_dict['energy_eq'] < -453.6,
+        fit_dict['volume_eq'] > 1208,
+        fit_dict['volume_eq'] < 1212,
+    ]
+
+
+def validate_elastic_constants_vinet(fit_dict):
+    print(fit_dict)
+    return [
+        fit_dict['b_prime_eq'] > 3.3,
+        fit_dict['b_prime_eq'] < 4.1,
+        fit_dict['bulkmodul_eq'] > 181,
+        fit_dict['bulkmodul_eq'] < 188,
+        fit_dict['energy_eq'] > -453.9,
+        fit_dict['energy_eq'] < -453.6,
+        fit_dict['volume_eq'] > 1208,
+        fit_dict['volume_eq'] < 1212,
+    ]
+
+
+class TestEvCurePoly(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         count_lst = [22, 22, 22, 21, 21]
@@ -71,7 +141,7 @@ class TestIntegration(unittest.TestCase):
             axes=["x", "y", "z"],
             strains=None,
         )
-        self.assertTrue(all(validate_elastic_constants(fit_dict=ev_curve_fit_dict)))
+        self.assertTrue(all(validate_elastic_constants_poly(fit_dict=ev_curve_fit_dict)))
 
         # Finalize
         lmp.close()
@@ -95,7 +165,7 @@ class TestIntegration(unittest.TestCase):
             axes=["x", "y", "z"],
             strains=None,
         )
-        self.assertTrue(all(validate_elastic_constants(fit_dict=ev_curve_fit_dict)))
+        self.assertTrue(all(validate_elastic_constants_poly(fit_dict=ev_curve_fit_dict)))
 
     def test_example_evcurve_with_statement(self):
         with pyr.get_lammps_engine() as lmp:
@@ -120,7 +190,7 @@ class TestIntegration(unittest.TestCase):
             )
 
         self.assertEqual(len(structure_opt), sum(self.count_lst))
-        self.assertTrue(all(validate_elastic_constants(fit_dict=ev_curve_fit_dict)))
+        self.assertTrue(all(validate_elastic_constants_poly(fit_dict=ev_curve_fit_dict)))
 
     def test_example_evcurve_with_minimization(self):
         with pyr.get_lammps_engine() as lmp:
@@ -136,7 +206,184 @@ class TestIntegration(unittest.TestCase):
                 strains=None,
             )
 
-        self.assertTrue(all(validate_elastic_constants(fit_dict=ev_curve_fit_dict)))
+        self.assertTrue(all(validate_elastic_constants_poly(fit_dict=ev_curve_fit_dict)))
+
+
+class TestEquationOfState(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        count_lst = [22, 22, 22, 21, 21]
+        element_lst = ["Fe", "Ni", "Cr", "Co", "Cu"]
+        potential = '2021--Deluigi-O-R--Fe-Ni-Cr-Co-Cu--LAMMPS--ipr1'
+        resource_path = os.path.join(os.path.dirname(__file__), "static")
+
+        # Generate SQS Structure
+        structure = pyr.generate_sqs_structure(
+            structure_template=pyr.get_ase_bulk("Al", cubic=True).repeat([3, 3, 3]),
+            element_lst=element_lst,
+            count_lst=count_lst
+        )[0]
+
+        # Select potential
+        df_pot = pyr.get_potential_dataframe(
+            structure=structure,
+            resource_path=resource_path
+        )
+
+        # Assign variable
+        cls.df_pot_selected = df_pot[df_pot.Name == potential].iloc[0]
+        cls.structure = structure
+        cls.resource_path = resource_path
+        cls.potential = potential
+        cls.count_lst = count_lst
+
+    def test_structure(self):
+        self.assertEqual(len(self.structure), sum(self.count_lst))
+
+    def test_vinet(self):
+        # Initialize Engine
+        lmp = pyr.get_lammps_engine()
+
+        # Optimize Structure
+        structure_opt = pyr.optimize_structure(
+            lmp=lmp,
+            structure=self.structure.copy(),
+            potential_dataframe=self.df_pot_selected
+        )
+        self.assertEqual(len(structure_opt), sum(self.count_lst))
+
+        # Calculate Elastic Constants
+        ev_curve_fit_dict = pyr.calculate_energy_volume_curve(
+            structure=structure_opt,
+            potential_dataframe=self.df_pot_selected,
+            num_points=11,
+            fit_type="vinet",
+            fit_order=3,
+            vol_range=0.1,
+            axes=["x", "y", "z"],
+            strains=None,
+        )
+        print("vinet", ev_curve_fit_dict)
+        self.assertTrue(all(validate_elastic_constants_vinet(fit_dict=ev_curve_fit_dict)))
+
+        # Finalize
+        lmp.close()
+
+    def test_pouriertarantola(self):
+        # Initialize Engine
+        lmp = pyr.get_lammps_engine()
+
+        # Optimize Structure
+        structure_opt = pyr.optimize_structure(
+            lmp=lmp,
+            structure=self.structure.copy(),
+            potential_dataframe=self.df_pot_selected
+        )
+        self.assertEqual(len(structure_opt), sum(self.count_lst))
+
+        # Calculate Elastic Constants
+        ev_curve_fit_dict = pyr.calculate_energy_volume_curve(
+            structure=structure_opt,
+            potential_dataframe=self.df_pot_selected,
+            num_points=11,
+            fit_type="pouriertarantola",
+            fit_order=3,
+            vol_range=0.1,
+            axes=["x", "y", "z"],
+            strains=None,
+        )
+        print("pouriertarantola", ev_curve_fit_dict)
+        self.assertTrue(all(validate_elastic_constants_pouriertarantola(fit_dict=ev_curve_fit_dict)))
+
+        # Finalize
+        lmp.close()
+
+    def test_murnaghan(self):
+        # Initialize Engine
+        lmp = pyr.get_lammps_engine()
+
+        # Optimize Structure
+        structure_opt = pyr.optimize_structure(
+            lmp=lmp,
+            structure=self.structure.copy(),
+            potential_dataframe=self.df_pot_selected
+        )
+        self.assertEqual(len(structure_opt), sum(self.count_lst))
+
+        # Calculate Elastic Constants
+        ev_curve_fit_dict = pyr.calculate_energy_volume_curve(
+            structure=structure_opt,
+            potential_dataframe=self.df_pot_selected,
+            num_points=11,
+            fit_type="murnaghan",
+            fit_order=3,
+            vol_range=0.1,
+            axes=["x", "y", "z"],
+            strains=None,
+        )
+        print("murnaghan", ev_curve_fit_dict)
+        self.assertTrue(all(validate_elastic_constants_murnaghan(fit_dict=ev_curve_fit_dict)))
+
+        # Finalize
+        lmp.close()
+
+    def test_birchmurnaghan(self):
+        # Initialize Engine
+        lmp = pyr.get_lammps_engine()
+
+        # Optimize Structure
+        structure_opt = pyr.optimize_structure(
+            lmp=lmp,
+            structure=self.structure.copy(),
+            potential_dataframe=self.df_pot_selected
+        )
+        self.assertEqual(len(structure_opt), sum(self.count_lst))
+
+        # Calculate Elastic Constants
+        ev_curve_fit_dict = pyr.calculate_energy_volume_curve(
+            structure=structure_opt,
+            potential_dataframe=self.df_pot_selected,
+            num_points=11,
+            fit_type="birchmurnaghan",
+            fit_order=3,
+            vol_range=0.1,
+            axes=["x", "y", "z"],
+            strains=None,
+        )
+        print("birchmurnaghan", ev_curve_fit_dict)
+        self.assertTrue(all(validate_elastic_constants_birchmurnaghan(fit_dict=ev_curve_fit_dict)))
+
+        # Finalize
+        lmp.close()
+
+    def test_birch(self):
+        # Initialize Engine
+        lmp = pyr.get_lammps_engine()
+
+        # Optimize Structure
+        structure_opt = pyr.optimize_structure(
+            lmp=lmp,
+            structure=self.structure.copy(),
+            potential_dataframe=self.df_pot_selected
+        )
+        self.assertEqual(len(structure_opt), sum(self.count_lst))
+
+        # Calculate Elastic Constants
+        ev_curve_fit_dict = pyr.calculate_energy_volume_curve(
+            structure=structure_opt,
+            potential_dataframe=self.df_pot_selected,
+            num_points=11,
+            fit_type="birch",
+            fit_order=3,
+            vol_range=0.1,
+            axes=["x", "y", "z"],
+            strains=None,
+        )
+        print("birch", ev_curve_fit_dict)
+        self.assertTrue(all(validate_elastic_constants_birch(fit_dict=ev_curve_fit_dict)))
+
+        # Finalize
+        lmp.close()
 
 
 if __name__ == '__main__':
