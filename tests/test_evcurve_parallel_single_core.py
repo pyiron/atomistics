36cab675ade0010066889d7e1f2c3c1eef2dca99
Jan Janssen <janssen@lanl.gov>
2023-08-15 14:34:49 -0600

    Extend range for bulk modulus derivate

--- tests/test_evcurve_parallel_single_core.py
+++ tests/test_evcurve_parallel_single_core.py
@@ -7,7 +7,7 @@ import structuretoolkit as stk
 
 def validate_fitdict(fit_dict):
     lst = [
-        fit_dict['b_prime_eq'] > 1.6,
+        fit_dict['b_prime_eq'] > 1.5,
         fit_dict['b_prime_eq'] < 3.0,
         fit_dict['bulkmodul_eq'] > 174,
         fit_dict['bulkmodul_eq'] < 184,
