f3cc5ffa0857ce7b091c7173e27d1a54ace88c26
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-08-18 11:19:37 -0600

    Update __init__.py

--- atomistics/__init__.py
+++ atomistics/__init__.py
@@ -3,6 +3,6 @@ from atomistics.elastic.calculator import ElasticMatrixCalculator
 from atomistics.evcurve.calculator import EnergyVolumeCurveCalculator
 
 # Additional modules
-from pyiron_lammps.workflows.thermo.thermo import get_thermo_bulk_model
-from pyiron_lammps.workflows.thermo.debye import get_debye_model
-from pyiron_lammps.workflows.evcurve.fit import get_energy_volume_curve_fit
+from atomistics.thermo.thermo import get_thermo_bulk_model
+from atomistics.thermo.debye import get_debye_model
+from atomistics.evcurve.fit import get_energy_volume_curve_fit
