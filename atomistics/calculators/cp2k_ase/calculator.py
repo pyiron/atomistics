7523967ee05e496a3efcc8a954207e3466f22a1f
Jan Janssen <jan.janssen@outlook.com>
2023-09-08 16:14:53 +0200

    Add interface for cp2k

---
+++ atomistics/calculators/cp2k_ase/calculator.py
@@ -0,0 +1,52 @@
+from ase.calculators.cp2k import CP2K
+
+
+def get_potential_energy_from_cp2k(structure, command='cp2k_shell.sopt', basis_set_file='./BASIS_SET', basis_set="DZVP-GTH-PADE", potential_file='./GTH_POTENTIALS', pseudo_potential="GTH-PADE-q4"):
+    structure.calc = CP2K(
+        command=command,
+        basis_set_file=basis_set_file,
+        basis_set=basis_set,
+        potential_file=potential_file,
+        pseudo_potential=pseudo_potential,
+    )
+    return structure.get_potential_energy()
+
+
+def get_forces_from_cp2k(structure, command='cp2k_shell.sopt', basis_set_file='./BASIS_SET', basis_set="DZVP-GTH-PADE", potential_file='./GTH_POTENTIALS', pseudo_potential="GTH-PADE-q4"):
+    structure.calc = CP2K(
+        command=command,
+        basis_set_file=basis_set_file,
+        basis_set=basis_set,
+        potential_file=potential_file,
+        pseudo_potential=pseudo_potential,
+    )
+    return structure.get_forces()
+
+
+def evaluate_with_cp2k(task_dict, command='cp2k_shell.sopt', basis_set_file='./BASIS_SET', basis_set="DZVP-GTH-PADE", potential_file='./GTH_POTENTIALS', pseudo_potential="GTH-PADE-q4"):
+    result_dict = {}
+    if "calc_energy" in task_dict.keys():
+        result_dict["energy"] = {
+            k: get_potential_energy_from_cp2k(
+                structure=v,
+                command=command,
+                basis_set_file=basis_set_file,
+                basis_set=basis_set,
+                potential_file=potential_file,
+                pseudo_potential=pseudo_potential,
+            )
+            for k, v in task_dict["calc_energy"].items()
+        }
+    if "calc_forces" in task_dict.keys():
+        result_dict["forces"] = {
+            k: get_forces_from_cp2k(
+                structure=v,
+                command=command,
+                basis_set_file=basis_set_file,
+                basis_set=basis_set,
+                potential_file=potential_file,
+                pseudo_potential=pseudo_potential,
+            )
+            for k, v in task_dict["calc_forces"].items()
+        }
+    return result_dict
---
+++ tests/test_phonons_cp2k.py
@@ -0,0 +1,51 @@
+import os
+import shutil
+
+from ase.build import bulk
+from phonopy.units import VaspToTHz
+import unittest
+
+from atomistics.calculators.cp2k_ase.calculator import evaluate_with_cp2k
+from atomistics.workflows.phonons.workflow import PhonopyWorkflow
+
+
+cp2k_command = "cp2k_shell.sopt"
+if shutil.which(cp2k_command) is not None:
+    skip_cp2k_test = False
+else:
+    skip_cp2k_test = True
+
+
+@unittest.skipIf(
+    skip_cp2k_test, "cp2k is not installed, so the cp2k tests are skipped."
+)
+class TestPhonons(unittest.TestCase):
+    def test_calc_phonons(self):
+        resource_path = os.path.join(os.path.dirname(__file__), "static", "cp2k")
+        calculator = PhonopyWorkflow(
+            structure=bulk("Al", a=4.05, cubic=True),
+            interaction_range=10,
+            factor=VaspToTHz,
+            displacement=0.01,
+            dos_mesh=20,
+            primitive_matrix=None,
+            number_of_snapshots=None,
+        )
+        structure_dict = calculator.generate_structures()
+        result_dict = evaluate_with_cp2k(
+            task_dict=structure_dict,
+            command=cp2k_command,
+            basis_set_file=os.path.join(resource_path, 'BASIS_SET'),
+            basis_set="DZVP-GTH-PADE",
+            potential_file=os.path.join(resource_path, 'GTH_POTENTIALS'),
+            pseudo_potential="GTH-PADE-q4",
+        )
+        mesh_dict, dos_dict = calculator.analyse_structures(output_dict=result_dict)
+        self.assertEqual((324, 324), calculator.get_hesse_matrix().shape)
+        self.assertTrue('qpoints' in mesh_dict.keys())
+        self.assertTrue('weights' in mesh_dict.keys())
+        self.assertTrue('frequencies' in mesh_dict.keys())
+        self.assertTrue('eigenvectors' in mesh_dict.keys())
+        self.assertTrue('group_velocities' in mesh_dict.keys())
+        self.assertTrue('frequency_points' in dos_dict.keys())
+        self.assertTrue('total_dos' in dos_dict.keys())
