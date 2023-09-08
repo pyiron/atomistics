04f46ce62ae69c2eb6673ae7bfc3ab153f5ef56b
Jan Janssen <jan.janssen@outlook.com>
2023-09-08 17:13:47 +0200

    black formatting

--- atomistics/calculators/quantumespresso_ase/calculator.py
+++ atomistics/calculators/quantumespresso_ase/calculator.py
@@ -1,27 +1,27 @@
 from ase.calculators.espresso import Espresso
 
 
-def get_potential_energy_from_qe(structure, pseudopotentials, tstress=True, tprnfor=True, kpts=(5, 5, 5)):
+def get_potential_energy_from_qe(
+    structure, pseudopotentials, tstress=True, tprnfor=True, kpts=(5, 5, 5)
+):
     structure.calc = Espresso(
-        pseudopotentials=pseudopotentials,
-        tstress=tstress,
-        tprnfor=tprnfor,
-        kpts=kpts
+        pseudopotentials=pseudopotentials, tstress=tstress, tprnfor=tprnfor, kpts=kpts
     )
     return structure.get_potential_energy()
 
 
-def get_forces_from_qe(structure, pseudopotentials, tstress=True, tprnfor=True, kpts=(5, 5, 5)):
+def get_forces_from_qe(
+    structure, pseudopotentials, tstress=True, tprnfor=True, kpts=(5, 5, 5)
+):
     structure.calc = Espresso(
-        pseudopotentials=pseudopotentials,
-        tstress=tstress,
-        tprnfor=tprnfor,
-        kpts=kpts
+        pseudopotentials=pseudopotentials, tstress=tstress, tprnfor=tprnfor, kpts=kpts
     )
     return structure.get_forces()
 
 
-def evaluate_with_quantumespresso(task_dict, pseudopotentials, tstress=True, tprnfor=True, kpts=(5, 5, 5)):
+def evaluate_with_quantumespresso(
+    task_dict, pseudopotentials, tstress=True, tprnfor=True, kpts=(5, 5, 5)
+):
     result_dict = {}
     if "calc_energy" in task_dict.keys():
         result_dict["energy"] = {
@@ -30,7 +30,7 @@ def evaluate_with_quantumespresso(task_dict, pseudopotentials, tstress=True, tpr
                 pseudopotentials=pseudopotentials,
                 tstress=tstress,
                 tprnfor=tprnfor,
-                kpts=kpts
+                kpts=kpts,
             )
             for k, v in task_dict["calc_energy"].items()
         }
@@ -41,7 +41,7 @@ def evaluate_with_quantumespresso(task_dict, pseudopotentials, tstress=True, tpr
                 pseudopotentials=pseudopotentials,
                 tstress=tstress,
                 tprnfor=tprnfor,
-                kpts=kpts
+                kpts=kpts,
             )
             for k, v in task_dict["calc_forces"].items()
         }
