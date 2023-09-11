4aeaec38d07e18a869d9972a3ccecf6338b05cc7
Jan Janssen <jan.janssen@outlook.com>
2023-09-11 17:19:09 +0200

    black formatting

--- atomistics/calculators/ase_interface/calculator.py
+++ atomistics/calculators/ase_interface/calculator.py
@@ -13,20 +13,14 @@ def evaluate_with_ase(task_dict, ase_calculator_class, *args, **kwargs):
     if "calc_energy" in task_dict.keys():
         result_dict["energy"] = {
             k: get_potential_energy_from_ase(
-                structure=v,
-                ase_calculator_class=ase_calculator_class,
-                *args,
-                **kwargs
+                structure=v, ase_calculator_class=ase_calculator_class, *args, **kwargs
             )
             for k, v in task_dict["calc_energy"].items()
         }
     if "calc_forces" in task_dict.keys():
         result_dict["forces"] = {
             k: get_forces_from_ase(
-                structure=v,
-                ase_calculator_class=ase_calculator_class,
-                *args,
-                **kwargs
+                structure=v, ase_calculator_class=ase_calculator_class, *args, **kwargs
             )
             for k, v in task_dict["calc_forces"].items()
         }
