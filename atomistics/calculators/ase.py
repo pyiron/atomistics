74900e35fa31d796c5c34521d2379c882315dfb6
liamhuber <liamhuber@greyhavensolutions.com>
2023-09-11 14:36:42 -0700

    Apply wrapper to ASE

---
+++ atomistics/calculators/ase.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from atomistics.calculators.wrapper import task_evaluation
+
+if TYPE_CHECKING:
+    from ase import Atoms
+    from ase.calculators.calculator import Calculator as ASECalculator
+
+
+@task_evaluation
+def evaluate_with_ase(
+    structure: Atoms,
+    tasks: list[str],
+    ase_calculator: ASECalculator
+):
+    structure.calc = ase_calculator
+    results = {}
+    if "calc_energy" in tasks:
+        results["energy"] = structure.get_potential_energy()
+    if "calc_forces" in tasks:
+        results["forces"] = structure.get_forces()
+    return results
