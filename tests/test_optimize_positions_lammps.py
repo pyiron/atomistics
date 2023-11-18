2ca6b072d241c0856ef6a74daceff524e8a86b60
Jan Janßen <janssen@mpie.de>
2023-11-18 08:05:57 +0100

    Use LAMMPS potentials provided by iprpy-data conda package

--- tests/test_optimize_positions_lammps.py
+++ tests/test_optimize_positions_lammps.py
@@ -16,28 +16,36 @@ except ImportError:
     skip_lammps_test = True
 
 
+def optimize_structure(structure, potential_name, resource_path=None):
+    df_pot = get_potential_dataframe(
+        structure=structure,
+        resource_path=resource_path
+    )
+    df_pot_selected = df_pot[df_pot.Name == potential_name].iloc[0]
+    task_dict = optimize_positions(structure=structure)
+    result_dict = evaluate_with_lammps(
+        task_dict=task_dict,
+        potential_dataframe=df_pot_selected,
+        lmp_optimizer_kwargs={"ftol": 0.000001},
+    )
+    return result_dict["structure_with_optimized_positions"]
+
+
 @unittest.skipIf(
     skip_lammps_test, "LAMMPS is not installed, so the LAMMPS tests are skipped."
 )
 class TestOptimizePositionsLAMMPS(unittest.TestCase):
-    def test_optimize_positions(self):
-        potential = '1999--Mishin-Y--Al--LAMMPS--ipr1'
+    def test_optimize_positions_with_resource_path(self):
+        potential_name = '1999--Mishin-Y--Al--LAMMPS--ipr1'
         resource_path = os.path.join(os.path.dirname(__file__), "static", "lammps")
         structure = bulk("Al", cubic=True)
         positions_before_displacement = structure.positions.copy()
         structure.positions[0] += [0.01, 0.01, 0.01]
-        df_pot = get_potential_dataframe(
+        structure_optimized = optimize_structure(
             structure=structure,
-            resource_path=resource_path
+            potential_name=potential_name,
+            resource_path=resource_path,
         )
-        df_pot_selected = df_pot[df_pot.Name == potential].iloc[0]
-        task_dict = optimize_positions(structure=structure)
-        result_dict = evaluate_with_lammps(
-            task_dict=task_dict,
-            potential_dataframe=df_pot_selected,
-            lmp_optimizer_kwargs={"ftol": 0.000001},
-        )
-        structure_optimized = result_dict["structure_with_optimized_positions"]
         self.assertTrue(
             all(np.isclose(
                 positions_before_displacement,
@@ -45,3 +53,19 @@ class TestOptimizePositionsLAMMPS(unittest.TestCase):
             ).flatten())
         )
 
+    def test_optimize_positions_without_resource_path(self):
+        potential_name = '1999--Mishin-Y--Al--LAMMPS--ipr1'
+        structure = bulk("Al", cubic=True)
+        positions_before_displacement = structure.positions.copy()
+        structure.positions[0] += [0.01, 0.01, 0.01]
+        structure_optimized = optimize_structure(
+            structure=structure,
+            potential_name=potential_name,
+            resource_path=None,
+        )
+        self.assertTrue(
+            all(np.isclose(
+                positions_before_displacement,
+                structure_optimized.positions-structure_optimized.positions[0],
+            ).flatten())
+        )
