bc16950570a8a6204e54095f405bfe1449f44a13
Jan Janssen <jan-janssen@users.noreply.github.com>
2023-12-01 07:40:05 +0100

    Update qe.py

--- atomistics/calculators/qe.py
+++ atomistics/calculators/qe.py
@@ -12,13 +12,11 @@ from atomistics.calculators.wrapper import as_task_dict_evaluator
 
 
 def call_qe_via_ase_command(calculation_name, working_directory):
-    env = os.environ
     subprocess.check_output(
-        env["ASE_ESPRESSO_COMMAND"].replace("PREFIX", calculation_name),
+        os.environ["ASE_ESPRESSO_COMMAND"].replace("PREFIX", calculation_name),
         shell=True,
         universal_newlines=True,
         cwd=working_directory,
-        env=env,
     )
 
 
