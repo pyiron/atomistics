a2ffeb723df60c23de432d3321f7c14074288245
Jan Janßen <janssen@mpie.de>
2023-12-18 19:15:37 +0100

    Add quasi harmonic and molecular dynamics test

--- tests/test_ase_md_matgl.py
+++ tests/test_ase_md_matgl.py
@@ -1,16 +1,28 @@
 from ase import units
 from ase.build import bulk
-from ase.calculators.emt import EMT
 from atomistics.calculators import calc_molecular_dynamics_langevin_with_ase
 import unittest
 
 
+try:
+    import matgl
+    from matgl.ext.ase import M3GNetCalculator
+
+    skip_matgl_test = False
+except ImportError:
+    skip_matgl_test = True
+
+
+@unittest.skipIf(
+    skip_matgl_test, "matgl is not installed, so the matgl tests are skipped."
+)
 class TestASEMD(unittest.TestCase):
     def test_ase_langevin(self):
         structure = bulk("Al", cubic=True).repeat([2, 2, 2])
+        ase_calculator = M3GNetCalculator(matgl.load_model("M3GNet-MP-2021.2.8-PES"))
         result_dict = calc_molecular_dynamics_langevin_with_ase(
             structure=structure,
-            ase_calculator=EMT(),
+            ase_calculator=ase_calculator,
             run=100,
             thermo=10,
             timestep=1 * units.fs,
