bbd6e9922937428df9eea4e6337358a3af71efc1
Jan Janssen <janssen@mpie.de>
2024-01-05 23:36:08 +0100

    fix thermal expasion

--- atomistics/shared/thermal_expansion.py
+++ atomistics/shared/thermal_expansion.py
@@ -1,7 +1,7 @@
 from atomistics.shared.output import OutputThermalExpansion
 
 
-class ThermalExpansionProperties:
+class ThermalExpansionProperties(OutputThermalExpansion):
     def __init__(self, temperatures_lst, volumes_lst):
         self._temperatures_lst = temperatures_lst
         self._volumes_lst = volumes_lst
@@ -14,10 +14,6 @@ class ThermalExpansionProperties:
 
 
 def get_thermal_expansion_output(temperatures_lst, volumes_lst, output):
-    thermal_properties = ThermalExpansionProperties(
+    return ThermalExpansionProperties(
         temperatures_lst=temperatures_lst, volumes_lst=volumes_lst
-    )
-    return OutputThermalExpansion(
-        temperatures=thermal_properties.temperatures,
-        volumes=thermal_properties.volumes,
-    ).get(output=output)
+    ).get_output(output=output)
