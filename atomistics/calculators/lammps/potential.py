d04f58eb7da4af21e9f64c1cdbd19d925d6a086a
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-03-14 14:20:50 -0500

    Update atomistics/calculators/lammps/potential.py
    Co-authored-by: Liam Huber <liamhuber@greyhavensolutions.com>

--- atomistics/calculators/lammps/potential.py
+++ atomistics/calculators/lammps/potential.py
@@ -164,7 +164,7 @@ class LammpsPotentialFile(PotentialAbstract):
             ]
         return None
 
-    def find_default(self, element: Union[set, list, str]) -> pandas.DataFrame:
+    def find_default(self, element: Union[set[str], list[str], str]) -> pandas.DataFrame:
         """
         Find the potentials
 
