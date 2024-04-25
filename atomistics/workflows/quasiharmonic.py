12cc6c3e7cf72520627b09fa0a3b6bff81d6eb49
Jan Janssen <janssen@mpie.de>
2024-04-25 08:48:05 -0500

    fixes

--- atomistics/workflows/quasiharmonic.py
+++ atomistics/workflows/quasiharmonic.py
@@ -68,8 +68,7 @@ def get_thermal_properties(
         repeat_vector
     )
     eng_internal_dict = {
-        key: value / np.prod(repeat_vector)
-        for key, value in eng_internal_dict.items()
+        key: value / np.prod(repeat_vector) for key, value in eng_internal_dict.items()
     }
     if quantum_mechanical:
         tp_collect_dict = _get_thermal_properties_quantum_mechanical(
