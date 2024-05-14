238f40121636c0364f037b80b919ba5606b0d085
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-05-14 14:13:24 -0500

    Update __init__.py

--- atomistics/calculators/__init__.py
+++ atomistics/calculators/__init__.py
@@ -9,12 +9,31 @@ from atomistics.calculators.ase import (
 )
 from atomistics.calculators.hessian import evaluate_with_hessian
 
+
+__all__ = [
+    calc_molecular_dynamics_langevin_with_ase,
+    calc_molecular_dynamics_npt_with_ase,
+    calc_molecular_dynamics_thermal_expansion_with_ase,
+    calc_static_with_ase,
+    evaluate_with_ase,
+    optimize_positions_with_ase,
+    optimize_positions_and_volume_with_ase,
+    evaluate_with_hessian,
+]
+
+
 try:
     from atomistics.calculators.qe import (
         calc_static_with_qe,
         evaluate_with_qe,
         optimize_positions_and_volume_with_qe,
     )
+
+    __all__ += [
+        calc_static_with_qe,
+        evaluate_with_qe,
+        optimize_positions_and_volume_with_qe,
+    ]
 except ImportError:
     pass
 
@@ -33,6 +52,21 @@ try:
         optimize_positions_and_volume_with_lammps,
         optimize_positions_with_lammps,
     )
+
+    __all__ += [
+        calc_molecular_dynamics_thermal_expansion_with_lammps,
+        calc_molecular_dynamics_nph_with_lammps,
+        calc_molecular_dynamics_npt_with_lammps,
+        calc_molecular_dynamics_nvt_with_lammps,
+        calc_molecular_dynamics_langevin_with_lammps,
+        calc_static_with_lammps,
+        evaluate_with_lammps,
+        evaluate_with_lammps_library,
+        get_potential_dataframe,
+        get_potential_by_name,
+        optimize_positions_and_volume_with_lammps,
+        optimize_positions_with_lammps,
+    ]
 except ImportError:
     pass
 
@@ -40,33 +74,7 @@ try:
     from atomistics.calculators.lammps.phonon import (
         calc_molecular_dynamics_phonons_with_lammps,
     )
+
+    __all__ += [calc_molecular_dynamics_phonons_with_lammps]
 except ImportError:
     pass
-
-
-__all__ = [
-    calc_molecular_dynamics_langevin_with_ase,
-    calc_molecular_dynamics_npt_with_ase,
-    calc_molecular_dynamics_thermal_expansion_with_ase,
-    calc_static_with_ase,
-    evaluate_with_ase,
-    optimize_positions_with_ase,
-    optimize_positions_and_volume_with_ase,
-    evaluate_with_hessian,
-    calc_static_with_qe,
-    evaluate_with_qe,
-    optimize_positions_and_volume_with_qe,
-    calc_molecular_dynamics_thermal_expansion_with_lammps,
-    calc_molecular_dynamics_nph_with_lammps,
-    calc_molecular_dynamics_npt_with_lammps,
-    calc_molecular_dynamics_nvt_with_lammps,
-    calc_molecular_dynamics_langevin_with_lammps,
-    calc_static_with_lammps,
-    evaluate_with_lammps,
-    evaluate_with_lammps_library,
-    get_potential_dataframe,
-    get_potential_by_name,
-    optimize_positions_and_volume_with_lammps,
-    optimize_positions_with_lammps,
-    calc_molecular_dynamics_phonons_with_lammps,
-]
