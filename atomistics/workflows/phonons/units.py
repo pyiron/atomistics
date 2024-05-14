67afb6b08d6b1543e15126cacf5b4b1a9adf8e5e
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-05-14 13:53:51 -0500

    Update units.py

--- atomistics/workflows/phonons/units.py
+++ atomistics/workflows/phonons/units.py
@@ -1,4 +1,7 @@
 import scipy.constants
+from phonopy.units import VaspToTHz, THzToEv, EvTokJmol
 
 kJ_mol_to_eV = 1000 / scipy.constants.Avogadro / scipy.constants.electron_volt
 kb = scipy.constants.physical_constants["Boltzmann constant in eV/K"][0]
+
+__all__ = [kb, kJ_mol_to_eV, VaspToTHz, THzToEv, EvTokJmol]
