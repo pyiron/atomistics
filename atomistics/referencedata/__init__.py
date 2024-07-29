9f01b57fabf243df54db9e835a813dc3f4c2fcfe
Jan Janßen <janssen@mpie.de>
2024-07-29 22:13:30 +0200

    fix import

--- atomistics/referencedata/__init__.py
+++ atomistics/referencedata/__init__.py
@@ -1,17 +1,12 @@
-from atomistics.referencedata.wolfram import get_chemical_information_from_wolframalpha
 from atomistics.referencedata.wiki import get_experimental_elastic_property_wikipedia
 
 try:
-    from atomistics.referencedata.mendeleevdb import get_chemical_information_from_mendeleev
+    from atomistics.referencedata.mendeleevdb import get_chemical_information_from_mendeleev, get_chemical_information_from_wolframalpha
 except ImportError:
     __all__ = []
 else:
-    __all__ = [get_chemical_information_from_mendeleev]
+    __all__ = [get_chemical_information_from_mendeleev, get_chemical_information_from_wolframalpha]
 
 
-__all__ += [
-    get_chemical_information_from_mendeleev,
-    get_chemical_information_from_wolframalpha,
-    get_experimental_elastic_property_wikipedia,
-]
+__all__ += [get_experimental_elastic_property_wikipedia]
 
