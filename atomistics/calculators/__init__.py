e166164e38c7780644e510089e50a9a1ac7506ed
Jan Janßen <janssen@mpie.de>
2025-02-23 17:42:06 +0100

    fixes

--- atomistics/calculators/__init__.py
+++ atomistics/calculators/__init__.py
@@ -53,18 +53,18 @@ try:
     )
 
     __all__ += [
-        "calc_molecular_dynamics_thermal_expansion_with_lammps",
-        "calc_molecular_dynamics_nph_with_lammps",
-        "calc_molecular_dynamics_npt_with_lammps",
-        "calc_molecular_dynamics_nvt_with_lammps",
-        "calc_molecular_dynamics_langevin_with_lammps",
-        "calc_static_with_lammps",
-        "evaluate_with_lammps",
-        "evaluate_with_lammps_library",
+        "calc_molecular_dynamics_thermal_expansion_with_lammpslib",
+        "calc_molecular_dynamics_nph_with_lammpslib",
+        "calc_molecular_dynamics_npt_with_lammpslib",
+        "calc_molecular_dynamics_nvt_with_lammpslib",
+        "calc_molecular_dynamics_langevin_with_lammpslib",
+        "calc_static_with_lammpslib",
+        "evaluate_with_lammpslib",
+        "evaluate_with_lammpslib_library_interface",
         "get_potential_dataframe",
         "get_potential_by_name",
-        "optimize_positions_and_volume_with_lammps",
-        "optimize_positions_with_lammps",
+        "optimize_positions_and_volume_with_lammpslib",
+        "optimize_positions_with_lammpslib",
     ]
 except ImportError:
     pass
@@ -74,6 +74,6 @@ try:
         calc_molecular_dynamics_phonons_with_lammpslib,
     )
 
-    __all__ += ["calc_molecular_dynamics_phonons_with_lammps"]
+    __all__ += ["calc_molecular_dynamics_phonons_with_lammpslib"]
 except ImportError:
     pass
