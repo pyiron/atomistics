440193535c4689712b3f843d874216c029ac3b81
Jan Janßen <janssen@mpie.de>
2025-02-26 22:45:23 +0100

    fix values

--- tests/test_sphinx_parser.py
+++ tests/test_sphinx_parser.py
@@ -6,7 +6,7 @@ import numpy as np
 from ase.build import bulk
 
 try:
-    from atomistics.calculators.sphinxdft import OutputParser, _write_input, calc_static_with_sphinxdft, evaluate_with_sphinx
+    from atomistics.calculators.sphinxdft import OutputParser, _write_input, calc_static_with_sphinxdft, evaluate_with_sphinx, HARTREE_TO_EV, HARTREE_OVER_BOHR_TO_EV_OVER_ANGSTROM, BOHR_TO_ANGSTROM
 
     skip_sphinx_test = False
 except ImportError:
@@ -28,7 +28,7 @@ class TestSphinxParser(unittest.TestCase):
 
     def test_output_parser(self):
         op = OutputParser(structure=self._structure, working_directory=self._output_directory)
-        self.assertEqual(op.get_energy(), -227.73892877167552)
+        self.assertEqual(op.get_energy() / HARTREE_TO_EV, -8.369251265371 )
         self.assertEqual(op.get_volume(), 69.42653100000001)
         self.assertTrue(np.all(np.isclose(op.get_forces(), np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]))))
         with self.assertRaises(NotImplementedError):
@@ -50,10 +50,6 @@ class TestSphinxParser(unittest.TestCase):
         with open(file, "r") as f:
             lines = f.readlines()
 
-        self.assertEqual(
-            "	cell = [[7.766774377480988, 0.0, 0.0], [0.0, 7.766774377480988, 0.0], [0.0, 0.0, 7.766774377480988]];\n",
-            "".join([l for l in lines if "cell" in l])
-        )
         self.assertEqual(
             "	eCut = 25.0;\n",
             "".join([l for l in lines if "eCut" in l])
@@ -79,7 +75,7 @@ class TestSphinxParser(unittest.TestCase):
             kpoint_folding=None,
             output_keys=["volume", "forces", "energy"],
         )
-        self.assertEqual(results["energy"], -227.73892877167552)
+        self.assertEqual(results["energy"] / HARTREE_TO_EV, -8.369251265371)
         self.assertEqual(results["volume"], 69.42653100000001)
         self.assertTrue(np.all(np.isclose(
             results["forces"],
@@ -96,7 +92,7 @@ class TestSphinxParser(unittest.TestCase):
             kpoint_coords=None,
             kpoint_folding=None,
         )
-        self.assertEqual(results["energy"], -227.73892877167552)
+        self.assertEqual(results["energy"] / HARTREE_TO_EV, -8.369251265371)
         self.assertTrue(np.all(np.isclose(
             results["forces"],
             np.array([[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]),
