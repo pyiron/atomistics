a286018691a5b001dddecefa652ab81e146b80dd
Jan Janßen <janssen@mpie.de>
2025-03-04 22:47:02 +0100

    more tests

--- tests/test_vasp_parser.py
+++ tests/test_vasp_parser.py
@@ -5,12 +5,9 @@ from unittest import TestCase
 from ase.build import bulk
 
 from atomistics.calculators.vasp import (
-    optimize_positions_with_vasp,
-    optimize_positions_and_volume_with_vasp,
-    calc_static_with_vasp,
     evaluate_with_vasp,
 )
-from atomistics.workflows import optimize_positions
+from atomistics.workflows import optimize_positions, optimize_positions_and_volume
 
 
 os.environ["VASP_PP_PATH"] = os.path.abspath(os.path.join(__file__, "..", "static", "vasp"))
@@ -24,7 +21,7 @@ class TestVaspParser(TestCase):
     def setUp(self):
         self._structure = bulk("Al", a=4.05, cubic=True)
 
-    def test_optimize_positions_with_vasp(self):
+    def test_optimize_positions_with_vasp_task(self):
         result = evaluate_with_vasp(
             task_dict=optimize_positions(self._structure),
             working_directory="opt_pos_task",
@@ -37,3 +34,36 @@ class TestVaspParser(TestCase):
             xc="pbe"
         )
         self.assertEqual(len(result), 1)
+
+    def test_optimize_positions_and_volume_with_vasp_task(self):
+        result = evaluate_with_vasp(
+            task_dict=optimize_positions_and_volume(self._structure),
+            working_directory="opt_vol_task",
+            executable_function=copy_file_funct,
+            prec="Accurate",
+            algo="Fast",
+            lreal=False,
+            lwave=False,
+            lorbit=0,
+            xc="pbe"
+        )
+        self.assertEqual(len(result), 1)
+
+    def test_calc_static_task(self):
+        result = evaluate_with_vasp(
+            task_dict={
+                "calc_energy": self._structure,
+                "calc_forces": self._structure,
+                "calc_stress": self._structure,
+                "calc_volume": self._structure,
+            },
+            working_directory="opt_vol_task",
+            executable_function=copy_file_funct,
+            prec="Accurate",
+            algo="Fast",
+            lreal=False,
+            lwave=False,
+            lorbit=0,
+            xc="pbe"
+        )
+        self.assertEqual(len(result), 4)
