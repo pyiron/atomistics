8db17cb13951f3c49d56a48b1076876c97f4356d
Jan Janßen <janssen@mpie.de>
2025-03-06 12:03:22 +0100

    another fix

--- atomistics/calculators/__init__.py
+++ atomistics/calculators/__init__.py
@@ -106,7 +106,7 @@ try:
         "evaluate_with_vasp",
         "calc_molecular_dynamics_vasp",
         "calc_static_with_vasp",
-        "optimize_positions_and_volume_vasp",
+        "optimize_positions_and_volume_with_vasp",
         "optimize_positions_with_vasp",
         "optimize_cell_with_vasp",
         "optimize_volume_with_vasp",
