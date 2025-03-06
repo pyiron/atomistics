cac25c57df0431aef2b36af4315e148dee709d33
Jan Janßen <janssen@mpie.de>
2025-03-06 12:33:41 +0100

    extend tests

--- tests/test_vasp_parser.py
+++ tests/test_vasp_parser.py
@@ -6,6 +6,10 @@ from ase.build import bulk
 
 from atomistics.calculators import (
     evaluate_with_vasp,
+    optimize_cell_with_vasp,
+    optimize_volume_with_vasp,
+    optimize_positions_with_vasp,
+    optimize_positions_and_volume_with_vasp,
 )
 from atomistics.workflows import optimize_positions, optimize_positions_and_volume, optimize_volume, optimize_cell
 
@@ -35,6 +39,20 @@ class TestVaspParser(TestCase):
         )
         self.assertEqual(len(result), 1)
 
+    def test_optimize_positions_with_vasp(self):
+        result = optimize_positions_with_vasp(
+            structure=self._structure,
+            working_directory="opt_pos_task",
+            executable_function=copy_file_funct,
+            prec="Accurate",
+            algo="Fast",
+            lreal=False,
+            lwave=False,
+            lorbit=0,
+            xc="pbe"
+        )
+        self.assertEqual(len(result), 2)
+
     def test_optimize_positions_and_volume_with_vasp_task(self):
         result = evaluate_with_vasp(
             task_dict=optimize_positions_and_volume(self._structure),
@@ -48,6 +66,20 @@ class TestVaspParser(TestCase):
             xc="pbe"
         )
         self.assertEqual(len(result), 1)
+
+    def test_optimize_positions_and_volume_with_vasp(self):
+        result = optimize_positions_and_volume_with_vasp(
+            structure=self._structure,
+            working_directory="opt_pos_and_vol_task",
+            executable_function=copy_file_funct,
+            prec="Accurate",
+            algo="Fast",
+            lreal=False,
+            lwave=False,
+            lorbit=0,
+            xc="pbe"
+        )
+        self.assertEqual(len(result), 2)
 
     def test_optimize_cell_with_vasp_task(self):
         result = evaluate_with_vasp(
@@ -63,6 +95,20 @@ class TestVaspParser(TestCase):
         )
         self.assertEqual(len(result), 1)
 
+    def test_optimize_cell_with_vasp(self):
+        result = optimize_cell_with_vasp(
+            structure=self._structure,
+            working_directory="opt_cell_task",
+            executable_function=copy_file_funct,
+            prec="Accurate",
+            algo="Fast",
+            lreal=False,
+            lwave=False,
+            lorbit=0,
+            xc="pbe"
+        )
+        self.assertEqual(len(result), 2)
+
     def test_optimize_volume_with_vasp_task(self):
         result = evaluate_with_vasp(
             task_dict=optimize_volume(self._structure),
@@ -76,6 +122,20 @@ class TestVaspParser(TestCase):
             xc="pbe"
         )
         self.assertEqual(len(result), 1)
+
+    def test_optimize_volume_with_vasp(self):
+        result = optimize_volume_with_vasp(
+            structure=self._structure,
+            working_directory="opt_vol_task",
+            executable_function=copy_file_funct,
+            prec="Accurate",
+            algo="Fast",
+            lreal=False,
+            lwave=False,
+            lorbit=0,
+            xc="pbe"
+        )
+        self.assertEqual(len(result), 2)
 
     def test_calc_static_task(self):
         result = evaluate_with_vasp(
