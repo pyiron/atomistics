ed2cc07325b030c80936937edff637dfb15d6368
Jan Janssen <janssen@mpie.de>
2025-11-04 10:18:23 +0100

    Thermal Expansion: Store changes in cell dimensions

--- atomistics/shared/thermal_expansion.py
+++ atomistics/shared/thermal_expansion.py
@@ -4,7 +4,7 @@ from atomistics.shared.output import OutputThermalExpansion
 
 
 class ThermalExpansionProperties:
-    def __init__(self, temperatures_lst: np.ndarray, volumes_lst: np.ndarray):
+    def __init__(self, cell_lst:np.ndarray, temperatures_lst: np.ndarray, volumes_lst: np.ndarray):
         """
         Initialize the ThermalExpansionProperties class.
 
@@ -12,8 +12,18 @@ class ThermalExpansionProperties:
         temperatures_lst (np.ndarray): Array of temperatures.
         volumes_lst (np.ndarray): Array of volumes.
         """
+        self._cell_lst = cell_lst
         self._temperatures_lst = temperatures_lst
         self._volumes_lst = volumes_lst
+
+    def cells(self) -> np.ndarray:
+        """
+        Get the array of cells.
+
+        Returns:
+        np.ndarray: Array of cells.
+        """
+        return self._cell_lst
 
     def volumes(self) -> np.ndarray:
         """
@@ -35,13 +45,14 @@ class ThermalExpansionProperties:
 
 
 def get_thermal_expansion_output(
-    temperatures_lst: np.ndarray, volumes_lst: np.ndarray, output_keys: tuple[str]
+    temperatures_lst: np.ndarray, volumes_lst: np.ndarray, cell_lst: np.ndarray, output_keys: tuple[str]
 ) -> dict:
     """
     Get the thermal expansion output.
 
     Parameters:
     temperatures_lst (np.ndarray): Array of temperatures.
+    cells_lst (np.ndarray): Array of cells.
     volumes_lst (np.ndarray): Array of volumes.
     output_keys (tuple[str]): Tuple of output keys.
 
@@ -49,7 +60,7 @@ def get_thermal_expansion_output(
     dict: Dictionary containing the thermal expansion output.
     """
     thermal = ThermalExpansionProperties(
-        temperatures_lst=temperatures_lst, volumes_lst=volumes_lst
+        cell_lst=cell_lst, temperatures_lst=temperatures_lst, volumes_lst=volumes_lst
     )
     return OutputThermalExpansion(
         **{k: getattr(thermal, k) for k in OutputThermalExpansion.keys()}
