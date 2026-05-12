8f33d6932c036e62ecb6274b5d0ddc584f24fa2e
pyiron-runner <pyiron@mpie.de>
2026-05-12 20:17:01 +0000

    Format black

--- src/atomistics/_version.py
+++ src/atomistics/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.dev1+g7c4d470b7'
-__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g7c4d470b7')
+__version__ = version = "0.1.dev1+g7c4d470b7"
+__version_tuple__ = version_tuple = (0, 1, "dev1", "g7c4d470b7")
 
 __commit_id__ = commit_id = None
