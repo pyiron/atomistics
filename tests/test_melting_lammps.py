d988f76e8c0adeef09baa7ce5441a802e4cc8c78
Jan Janssen <janssen@mpie.de>
2026-05-18 06:29:15 -0600

    Add a unit test to validate the structure size

--- tests/test_melting_lammps.py
+++ tests/test_melting_lammps.py
@@ -1,11 +1,13 @@
 import os
 
 import unittest
+from ase.build import bulk
+import numpy as np
 
 
 try:
     from atomistics.calculators import get_potential_by_name
-    from atomistics.calculators.lammps.melting import estimate_melting_temperature
+    from atomistics.calculators.lammps.melting import estimate_melting_temperature, _generate_structure_with_fixed_number_of_atoms
 
     skip_lammps_test = False
 except ImportError:
@@ -31,3 +33,23 @@ class TestLammpsMelting(unittest.TestCase):
             seed=None,
         )
         self.assertIn(melting_temp, [992, 1008, 1023, 1039])
+
+    def test_generate_structure_with_fixed_number_of_atoms(self):
+        structure_lst = [bulk("Al"), bulk("Al", cubic=True), bulk("Fe"), bulk("Fe", cubic=True), bulk("Mg"), bulk("Mg", orthorhombic=True), bulk("Si"), bulk("Si", orthorhombic=True)]
+        number_lst = [10, 100, 100]
+        new_lst = []
+        for s in structure_lst:
+            for n in number_lst:
+                new_lst.append(_generate_structure_with_fixed_number_of_atoms
+(structure=s, number_of_atoms=n))
+        result_lst = [
+            125, 125, 125, 
+            500, 500, 500, 
+            125, 125, 125, 
+            250, 250, 250, 
+            250, 250, 250, 
+            500, 500, 500,
+            250, 250, 250,
+            500, 500, 500
+        ]
+        self.assertEqual(result_lst, [len(s) for s in new_lst])
