6c0e0bad470388b0569de6d98417fb14a87f36b6
Sam Waseda <waseda@mpie.de>
2026-05-18 11:26:37 +0200

    Rephrase import warning because the messages are sometimes weird

--- src/atomistics/shared/import_warning.py
+++ src/atomistics/shared/import_warning.py
@@ -5,17 +5,15 @@ def raise_warning(module_list, import_error):
     if len(module_list) == 1:
         warnings.warn(
             message=module_list[0]
-            + "() is not available as the import of the"
-            + import_error.msg[2:]
-            + " failed.",
+            + "() is not available as the following import failed: "
+            + str(import_error),
             stacklevel=2,
         )
     else:
         error_msg = "(), ".join(module_list[:-1]) + "() and " + module_list[-1] + "()"
         warnings.warn(
             message=error_msg
-            + " are not available as the import of the"
-            + import_error.msg[2:]
-            + " failed.",
+            + " are not available as the following import failed: "
+            + str(import_error),
             stacklevel=2,
         )
